120654b71aa4991e22aa475ce86b293cd11f42f5
Tycho Grouwstra <tychogrouwstra@gmail.com>
2016-08-16 16:27:41 +0800

    quiz: refactor Parser

--- Parser.java
+++ Parser.java
@@ -1,42 +1,53 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+package quiz;
+import java.io.*;
 /**
- * This class is thread safe.
+ * This class is thread safe. It reads and writes string content from/to the specified file.
  */
 public class Parser {
-  private File file;
+
+  private final File file;
   public synchronized void setFile(File f) {
     file = f;
   }
   public synchronized File getFile() {
     return file;
   }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
+  
+  /**
+   * Reads the file to a string, optionally allowing unicode.
+   * @param allowUnicode whether to allow unicode
+   * @throws IOException
+   * @return String
+   */
+  public String getContent(boolean allowUnicode = false) throws IOException {
+    Reader inpReader = new InputStreamReader(new FileInputStream(file));
+    StringBuilder output = new StringBuilder("");
     int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    while ((data = inpReader.read()) >= 0) {
+      if (allowUnicode || isAscii(data)) {
+        output.append((char) data);
       }
     }
-    return output;
+    inpReader.close();
+    return output.toString();
   }
+  
+  /**
+   * Writes the content to the file.
+   * @param content the content to write to file
+   * @throws IOException
+   * @return void
+   */
   public void saveContent(String content) throws IOException {
     FileOutputStream o = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
   }
+  
+  private static final int MAX_ASCII = 0x80;
+  private static boolean isAscii(int data) {
+    return data < MAX_ASCII;
+  }
+  
 }
